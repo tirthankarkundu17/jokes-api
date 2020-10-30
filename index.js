d8aece4eb21028c54934443f9c236dbc0711d72a
Tirthankar Kundu <tirthankar.kundu94@gmail.com>
2020-10-31 00:42:54 +0530

    Initial commit

--- index.js
+++ index.js
@@ -1,45 +1,47 @@
-const express = require('express');
-const LimitingMiddleware = require('limiting-middleware');
-const { randomJoke, randomTen, jokeByType } = require('./handler');
+const express = require("express");
+const LimitingMiddleware = require("limiting-middleware");
+const { randomJoke, randomTen, jokeByType } = require("./handler");
 
 const app = express();
 
-app.use(new LimitingMiddleware().limitByIp());
+app.use(
+  new LimitingMiddleware({ limit: 10, resetInterval: 30000 }).limitByIp()
+);
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header("Access-Control-Allow-Origin", "*");
   next();
 });
 
-app.get('/', (req, res) => {
-  res.send('Try /random_joke, /random_ten, /jokes/random, or /jokes/ten');
+app.get("/", (req, res) => {
+  res.send("Try /random_joke, /random_ten, /jokes/random, or /jokes/ten");
 });
 
-app.get('/ping', (req, res) => {
-  res.send('pong');
+app.get("/ping", (req, res) => {
+  res.send("pong");
 });
 
-app.get('/random_joke', (req, res) => {
+app.get("/random_joke", (req, res) => {
   res.json(randomJoke());
 });
 
-app.get('/random_ten', (req, res) => {
+app.get("/random_ten", (req, res) => {
   res.json(randomTen());
 });
 
-app.get('/jokes/random', (req, res) => {
+app.get("/jokes/random", (req, res) => {
   res.json(randomJoke());
 });
 
-app.get('/jokes/ten', (req, res) => {
+app.get("/jokes/ten", (req, res) => {
   res.json(randomTen());
 });
 
-app.get('/jokes/:type/random', (req, res) => {
+app.get("/jokes/:type/random", (req, res) => {
   res.json(jokeByType(req.params.type, 1));
 });
 
-app.get('/jokes/:type/ten', (req, res) => {
+app.get("/jokes/:type/ten", (req, res) => {
   res.json(jokeByType(req.params.type, 10));
 });
 
@@ -47,7 +49,8 @@ app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
 
   res.status(statusCode).json({
-    type: 'error', message: err.message
+    type: "error",
+    message: err.message,
   });
 });
 
